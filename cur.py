67ab5e16b2adcab619c0de1df08d3c702c26c788
maydinn <muhammedaydin9214@gmail.com>
2023-08-02 07:07:34 +0200

    cm

---
+++ cur.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+import pandas as pd
+from prophet import Prophet
+import requests
+import io
+import matplotlib.pyplot as plt
+from prophet.plot import add_changepoints_to_plot
+import streamlit as st
+
+m = Prophet()
+m.fit(df)
+
+
+fig = m.plot(forecast)
+a = add_changepoints_to_plot(fig.gca(), m, forecast)
+
+ st.plotly_chart(a, 
+         use_container_width=True )
