9db5c3746e802b3cb6322d3cb73c878c4d2e7cc6
maydinn <muhammedaydin9214@gmail.com>
2023-10-16 20:43:01 +0200

    all

--- cur.py
+++ cur.py
@@ -129,9 +129,9 @@ fig = px.line(df, x='ds', y='y', title='Time Series Data')
 fig.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat'], mode='lines', name='Forecast'))
 
 changepoints = df_ny['ds'].values
-
+fig.add_vline((x=changepoints[0], line_width=3, line_dash="dash", line_color="green")
 for changepoint in changepoints:
-    fig.add_trace(go.Scatter(x=[changepoint, changepoint], y=[0, forecast.loc[forecast['ds'] == changepoint, 'yhat'].values[0]],
+    fig.add_trace(go.Scatter(x=[changepoint], y=[forecast.loc[forecast['ds'] == changepoint, 'yhat'].values[0]],
                              mode='lines',
                              line=dict(color='red', dash='dot'),
                              name='Changepoint'))
