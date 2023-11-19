2762a5da7c01992d09c8cb7a940c1ee728f80d3c
maydinn <muhammedaydin9214@gmail.com>
2023-11-19 15:00:37 +0100

    all

--- cur.py
+++ cur.py
@@ -176,11 +176,11 @@ with col1_x:
                                'Title': title_list,
                                'Web': web_list,
                                })
-            news['News Title'] = news[['Web', 'Title']].apply(lambda x: make_clickable(x['Web'], x['Title']), 1)
+            news['News Title'] = news[['Web', 'Web']].apply(lambda x: make_clickable(x['Web'], x['Title']), 1)
 
             news['Date'] = news['Date'].apply(lambda x: pd.to_datetime(x).strftime("%d %b, %Y"))
             news.index +=1
-            news_ = news[['Date', 'News Title', 'Web']]
+            news_ = news[['Date', 'Title', 'Web']]
             news_ = news_.to_html(escape=False, index=False)
             st.write(news_, unsafe_allow_html=True)
 #             st.markdown(
