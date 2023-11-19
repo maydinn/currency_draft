f0707b3f2b575fc4a76b66455276fd27ff246215
maydinn <muhammedaydin9214@gmail.com>
2023-11-19 14:36:18 +0100

    all

--- cur.py
+++ cur.py
@@ -182,7 +182,7 @@ with col1_x:
             news.index +=1
             news = news[['Date', 'URL']]
             #news = news.to_html(escape=False)
-            st.write(news)
+            st.table(news)
            
         if len(points_list) > 1:
             expand01 = df_m['date'].values[1]
