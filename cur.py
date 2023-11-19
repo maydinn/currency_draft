8bb9901d5567a219a8b8f7e37235adf4e2b2628f
maydinn <muhammedaydin9214@gmail.com>
2023-11-19 14:00:15 +0100

    all

--- cur.py
+++ cur.py
@@ -180,8 +180,8 @@ with col1_x:
 
             news['Date'] = news['Date'].apply(lambda x: pd.to_datetime(x).strftime("%d %b, %Y"))
             news.index +=1
-            news = news.to_html(escape=False)
             news = news[['Date', 'URL']]
+            news = news.to_html(escape=False)
             st.write(news, unsafe_allow_html=True)
             expand00 = df_m['date'].values[0]
         if len(points_list) > 1:
